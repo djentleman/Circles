ce2b26f0b6068f13a95d8af6e54cc6f6154790d8
Todd Perry <up612136@myport.ac.uk>
2013-06-11 16:54:20 +0100

    added corpse object
    not yet implemented

--- prototype 1/food.py
+++ prototype 1/food.py
@@ -50,3 +50,39 @@ class Food:
         self.body.setFill(color_rgb(0, self.nutrition, self.nutrition))
         #self.body.undraw()
         self.body.draw(self.environment)
+
+class Corpse: # should inhert food
+    # left behind after death
+    # slowly rots - always has the same nutritional value, but slowly
+    # becomes more dangerous to eat
+    def __init__(self, x, y, radius, environment):
+        self.x = x
+        self.y = y
+        self.nutrition = 120 # always 120
+        self.radius = radius
+        self.decay = 100 # slowly decays over time
+        self.eaten = False
+        self.body = Circle(Point(x, y), radius)
+        self.body.setFill(color_rgb(255, self.decay, self.decay))
+        self.body.draw(environment)
+
+    def decay(self):
+        self.decay -= 1
+        self.body.setFill(color_rgb(255, self.decay, self.decay))
+        self.body.draw(self.environment)
+
+    def getEaten(self):
+        if not self.eaten:
+            self.radius -= 0.05
+            #print(self.radius)
+
+            if self.radius < 0:
+                # food has died
+                self.body.undraw()
+                # remove from all lists
+                self.eaten = True
+            else:
+                self.body = Circle(Point(self.x, self.y), self.radius)
+                self.body.setFill(color_rgb(255, self.decay, self.decay))
+                self.body.undraw()
+                self.body.draw(self.environment)
